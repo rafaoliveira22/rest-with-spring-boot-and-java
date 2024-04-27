c9ffdf1ddd9dc120a6b334bf5026cf423d009f2b
Rafael Oliveira Souza <71654681+rafaoliveira22@users.noreply.github.com>
2024-04-27 18:25:27 -0300

    testes pro create, update e delete

--- rest-with-spring-boot-and-java/src/test/java/br/com/rafaoliveira/unittests/mockito/services/PersonServicesTest.java
+++ rest-with-spring-boot-and-java/src/test/java/br/com/rafaoliveira/unittests/mockito/services/PersonServicesTest.java
@@ -3,9 +3,9 @@ package br.com.rafaoliveira.unittests.mockito.services;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
-import java.util.List;
 import java.util.Optional;
 
+import br.com.rafaoliveira.data.vo.v1.PersonVO;
 import br.com.rafaoliveira.model.Person;
 import br.com.rafaoliveira.repositories.PersonRepository;
 import br.com.rafaoliveira.services.PersonServices;
@@ -39,10 +39,10 @@ public class PersonServicesTest {
 
     @Test
     public void testFindById(){
-        Person person = input.mockEntity(1);
-        person.setId(1L);
+        Person entity = input.mockEntity(1);
+        entity.setId(1L);
 
-        when(repository.findById(1L)).thenReturn(Optional.of(person));
+        when(repository.findById(1L)).thenReturn(Optional.of(entity));
 
         var result = service.findById(1L);
 
@@ -55,5 +55,66 @@ public class PersonServicesTest {
         assertEquals("First Name Test1", result.getFirstName());
         assertEquals("Last Name Test1", result.getLastName());
         assertEquals("Female", result.getGender());
+    }
+
+
+    @Test
+    public void testCreate(){
+        Person entity = input.mockEntity(1);
+        Person persisted = entity;
+        persisted.setId(1L);
+
+        PersonVO vo = input.mockVO(1);
+        vo.setKey(1L);
+
+        when(repository.save(entity)).thenReturn(persisted);
+
+        var result = service.create(vo);
+
+        assertNotNull(result);
+        assertNotNull(result.getKey());
+        assertNotNull(result.getLinks());
+
+        assertTrue(result.toString().contains("links: [</person/v1/1>;rel=\"self\"]"));
+        assertEquals("Addres Test1", result.getAddress());
+        assertEquals("First Name Test1", result.getFirstName());
+        assertEquals("Last Name Test1", result.getLastName());
+        assertEquals("Female", result.getGender());
+    }
+
+    @Test
+    public void testUpdate(){
+        Person entity = input.mockEntity(1);
+        entity.setId(1L);
+
+        Person persisted = entity;
+
+        PersonVO vo = input.mockVO(1);
+        vo.setKey(1L);
+
+        when(repository.save(entity)).thenReturn(persisted);
+        when(repository.findById(1L)).thenReturn(Optional.of(entity));
+
+        var result = service.update(vo);
+
+        assertNotNull(result);
+        assertNotNull(result.getKey());
+        assertNotNull(result.getLinks());
+
+        assertTrue(result.toString().contains("links: [</person/v1/1>;rel=\"self\"]"));
+        assertEquals("Addres Test1", result.getAddress());
+        assertEquals("First Name Test1", result.getFirstName());
+        assertEquals("Last Name Test1", result.getLastName());
+        assertEquals("Female", result.getGender());
+    }
+
+    @Test
+    public void testDelete(){
+        Person entity = input.mockEntity(1);
+        entity.setId(1L);
+
+        when(repository.findById(1L)).thenReturn(Optional.of(entity));
+
+        service.findById(1L);
     }
 }
