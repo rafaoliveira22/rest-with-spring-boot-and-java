4861913a191e7942382b2cc578c828495e786dac
Rafael Oliveira Souza <71654681+rafaoliveira22@users.noreply.github.com>
2024-10-19 20:15:56 -0300

    create table books

--- rest-with-spring-boot-and-java/src/main/resources/scripts/SCRIPT_BANCO_DE_DADOS.sql
+++ rest-with-spring-boot-and-java/src/main/resources/scripts/SCRIPT_BANCO_DE_DADOS.sql
@@ -12,6 +12,10 @@ DESC flyway_schema_history;
 SELECT * FROM flyway_schema_history;
 DROP TABLE flyway_schema_history;
 
+DESC books;
+SELECT * FROM books;
+
+
 DROP DATABASE rest_with_spring_boot_and_java;
 
 
